c230d26ee8800adaccb1eeec9a2d2e5761d969e4
Jonathan Tang <jonoymtang@gmail.com>
2021-09-11 19:08:32 +1000

    commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
     Link
 } from "react-router-dom";
 import Homepage from "./screens/homepage/Homepage";
+import Questions from "./screens/questions/Questions";
 // import Login from "./screens/login/Login"
 
 class App extends Component {
@@ -16,7 +17,7 @@ class App extends Component {
             <div>
                 <Switch>
                     <Route path="/">
-                        <Homepage />
+                        <Questions />
                     </Route>
                 </Switch>
             </div>
