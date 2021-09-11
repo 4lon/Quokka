459f55e0344374ee2ee21bc44c8fd616a6f627c7
Alon Nusem <61749872+4lon@users.noreply.github.com>
2021-09-11 14:57:15 +1000

    file layout update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {
     BrowserRouter as Router,
@@ -7,11 +6,19 @@ import {
     Route,
     Link
 } from "react-router-dom";
-import Homepage from "./screens/Homepage";
+import Homepage from "./screens/homepage/Homepage";
+// import Login from "./screens/login/Login"
 
 class App extends Component {
   render() {
     return (
+        // <div className="app">
+        //     <Router>
+        //         <Switch>
+        //             <Route exact path="/" component={Login} />
+        //         </Switch>
+        //     </Router>
+        // </div>
         <Router>
             <div>
                 <nav>
@@ -30,9 +37,13 @@ class App extends Component {
                     {/*</ul>*/}
                 </nav>
                 <Switch>
+                    {/*<Route exact path="/" component={Login} />*/}
                     <Route path="/">
                         <Homepage />
                     </Route>
+                    {/*<Route exact path="/login">*/}
+                    {/*    <Login />*/}
+                    {/*</Route>*/}
                 </Switch>
             </div>
         </Router>
